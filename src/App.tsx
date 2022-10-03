a08f23c2092894e361d4a3f17a70c5ac0ad3b985
Koki Takahashi <hakatasiloving@gmail.com>
2022-10-03 13:00:29 +0000

    Implement password prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal, JSX } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
+import {throttle} from 'lodash';
 
 const App: Component = () => {
+  const [password, setPassword] = createSignal('');
+
+  const onInputPassword: JSX.EventHandlerUnion<HTMLInputElement, Event> = (event) => {
+    setPassword((event.target as HTMLInputElement).value)
+    onChangePassword();
+  };
+
+  const onChangePassword = throttle(async () => {
+    if (password().match(/[^a-zA-Z0-9]/)) {
+      return;
+    }
+    const res = await fetch(`https://hakata-public.s3.ap-northeast-1.amazonaws.com/mmd-archive-thumbs/${password()}.json`);
+    const data = await res.json();
+  }, 500);
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <div class={styles.logo}/>
+        <p class={styles.hint}>
+          ☳☴☶☳☳☱☱☴☲☳☱☰☶☱☱☴☵☲☳☲☴☳☶☰☱☴☵☴☴☰☵☷☲☳☶☴☴☴☶☲☷☵☴☰☱☰☰☰
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <input type="text" class={styles.password} onInput={onInputPassword} value={password()}/>
       </header>
     </div>
   );
